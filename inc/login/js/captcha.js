5c79eb218f2500190ba9af63f7333cbd5a22b7e3
mnasikin <khoirunart45@gmail.com>
2023-10-02 20:13:00 +0700

    Minor update

--- inc/login/js/captcha.js
+++ inc/login/js/captcha.js
@@ -1,12 +1,12 @@
-// Fungsi JavaScript untuk melakukan validasi saat tombol "Enter" ditekan
+// Validate captcha when "enter" button pressed
 function validateForm() {
     var userAnswer = document.forms["captchaForm"]["captcha_answer"].value;
     var captchaAnswer = <?php echo $_SESSION['captcha_answer']; ?>;
     
     if (parseInt(userAnswer) === captchaAnswer) {
-        return true; // Izinkan pengiriman formulir
+        return true; // Allow send form
     } else {
         alert("CAPTCHA salah. Coba lagi.");
-        return false; // Mencegah pengiriman formulir
+        return false; // Disallow send form
     }
 }
