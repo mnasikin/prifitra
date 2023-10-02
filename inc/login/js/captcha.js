7e4c9d3808d62b0725aad14ef2b63a44ef72a151
mnasikin <66989363+mnasikin@users.noreply.github.com>
2023-10-02 20:09:03 +0700

    Update captcha.js

--- inc/login/js/captcha.js
+++ inc/login/js/captcha.js
@@ -1,12 +1,1 @@
-// Fungsi JavaScript untuk melakukan validasi saat tombol "Enter" ditekan
-function validateForm() {
-    var userAnswer = document.forms["captchaForm"]["captcha_answer"].value;
-    var captchaAnswer = <?php echo $_SESSION['captcha_answer']; ?>;
-    
-    if (parseInt(userAnswer) === captchaAnswer) {
-        return true; // Izinkan pengiriman formulir
-    } else {
-        alert("CAPTCHA salah. Coba lagi.");
-        return false; // Mencegah pengiriman formulir
-    }
-}
+
