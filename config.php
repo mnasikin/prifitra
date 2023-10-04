6aa96781ff965d777e2cce8742e24303ac4e379c
mnasikin <66989363+mnasikin@users.noreply.github.com>
2023-10-04 19:55:04 +0700

    update default config

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    define('BASE_URL', 'http://localhost/prifitra/');
+    define('BASE_URL', '');
 // Fill with your current installation url. Eg. http://localhost/prifitra or https://wpgan.com
 // or leave it empty to auto detect (not working in local environment)
 
