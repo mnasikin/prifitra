43a0e0c286ae4487828056c7e09a993d3b0dcc73
mnasikin <66989363+mnasikin@users.noreply.github.com>
2024-02-20 16:18:46 +0700

    Minor update

--- inc/main/lib/vars/global.php
+++ inc/main/lib/vars/global.php
@@ -3,5 +3,5 @@
     define('BASE_VER', '1.0');
     define('BASE_ICON', './inc/img/main/privitra-icon.png');
     define('BASE_TAG', 'Private File Transfer');
-    define('BASE_AVATAR', './inc/img/main/avatar.jpg');
+    define('BASE_AVATAR', './inc/img/main/privitra-icon.png');
 ?>
