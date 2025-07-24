db9578d40c462d32203e622f9a1dd99308b81be9
mnasikin <66989363+mnasikin@users.noreply.github.com>
2025-07-24 19:01:29 +0700

    Version tag update

--- inc/main/lib/vars/global.php
+++ inc/main/lib/vars/global.php
@@ -1,6 +1,6 @@
 <?php
     define('BASE_NAME', 'PRIFITRA');
-    define('BASE_VER', '2.0');
+    define('BASE_VER', '3.0');
     define('BASE_ICON', './inc/img/main/privitra-icon.png');
     define('BASE_TAG', 'Private File Transfer');
     define('BASE_AVATAR', './inc/img/main/privitra-icon.png');
